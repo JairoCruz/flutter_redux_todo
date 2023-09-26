bc5bd26cf6edbe06d55a2ed6da0cb8552e06e9f6
Jairo Cruz <azaveliz@example.com>
2023-09-25 20:38:02 -0600

    filter work fine

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:js_util';
+
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -30,7 +33,14 @@ class DeleteTodoAction {
   DeleteTodoAction(this.index);
 }
 
+class SetVisibilityFilterAction {
+  final VisibilityFilter filter;
+  SetVisibilityFilterAction(this.filter);
+}
+
 enum VisibilityFilter { showAll, showActive, showCompleted }
+
+//enum Filter { show, active, completed }
 
 class AppState {
   List<Todo> todos;
@@ -48,22 +58,35 @@ class AppState {
   }
 }
 
+///////
+typedef SetVisibilityFilterFunction = void Function(VisibilityFilter filter);
+
+typedef TodoTapFunction = void Function(int id);
+
+typedef void TodoTapDelete(int id);
+
+
 AppState todosReducer(AppState state, action) {
+  
   if (action is AddTodoAction) {
     return AppState(
         todos: List.from(state.todos)..add(action.todo),
         visibilityFilter: state.visibilityFilter);
   } else if (action is UpdateCompleteTodoAction) {
-    //
+    
     var newItem = state.todos[action.index];
     newItem.completed = !newItem.completed;
 
     return state.copywith(todos: state.todos);
   } else if (action is DeleteTodoAction) {
-    print('antes de ${state.todos.length}');
     state.todos.removeAt(action.index);
-    print('despues de ${state.todos.length}');
     return state.copywith(todos: state.todos);
+  } else if (action is SetVisibilityFilterAction) {
+    return AppState(
+      todos: state.todos,
+      visibilityFilter: action.filter
+    );
+    
   } else {
     return state;
   }
@@ -91,6 +114,162 @@ class TodoItem extends StatelessWidget {
   }
 }
 
+
+// ViewModel ListTodo
+class _ViewModel {
+  
+  final List<Todo> todos;
+  final TodoTapFunction onTodoTap;
+  final TodoTapDelete onTodoTapDelete;
+
+  _ViewModel({
+    required this.todos,
+    required this.onTodoTap,
+    required this.onTodoTapDelete
+  });
+
+  @override
+  bool operator ==(Object other) =>
+    identical(this, other) || other is _ViewModel &&
+      runtimeType == other.runtimeType &&
+      const ListEquality<Todo>().equals(todos, other.todos);
+
+
+  @override
+  int get hashCode => todos.hashCode;
+
+}
+
+// ViewModel FilterList
+class _ViewModelLC {
+
+ 
+  final SetVisibilityFilterFunction fill;
+
+  _ViewModelLC({
+    required this.fill
+  });
+
+
+   @override
+  bool operator ==(Object other);
+
+  @override
+  int get hashCode;
+}
+
+
+// Widget checkbox
+class FilterLink extends StatelessWidget {
+  
+  const FilterLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, _ViewModelLC>(
+      converter: (store) => _ViewModelLC(
+        fill: (f) => store.dispatch(SetVisibilityFilterAction(f))),
+      builder: (context, viewModel){
+        return PopupMenuButton<VisibilityFilter>(
+          initialValue: VisibilityFilter.showAll,
+          onSelected: (VisibilityFilter filter) {
+            viewModel.fill(filter);
+          },
+          itemBuilder: (context) => <PopupMenuEntry<VisibilityFilter>>[
+            const PopupMenuItem<VisibilityFilter>(
+              value: VisibilityFilter.showAll,
+              child: Text('All'),
+            ),
+            const PopupMenuItem(
+              value: VisibilityFilter.showActive,
+              child: Text('Active'),
+              ),
+            const PopupMenuItem(
+              value: VisibilityFilter.showCompleted,
+              child: Text('Completos')
+              )
+          ],
+        );
+      });
+  }
+
+}
+
+
+// Lista de todos With ViewModel
+
+class VisibleTodoList extends StatelessWidget {
+
+  List<Todo> _getVisibleTodos(List<Todo> todos, VisibilityFilter filter) {
+    print('antes de switch: $filter');
+    switch (filter) {
+      case VisibilityFilter.showAll:
+        return todos;
+      case VisibilityFilter.showCompleted:
+        return todos.where((todo) => todo.completed == true).toList();
+      case VisibilityFilter.showActive:
+        return todos.where((todo) => todo.completed == false).toList();
+      default:
+        return <Todo>[];
+    }
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, _ViewModel>(
+      //distinct: true,
+      converter: (store) => _ViewModel(
+        todos: _getVisibleTodos(store.state.todos, store.state.visibilityFilter),
+        onTodoTap: (id) => store.dispatch(UpdateCompleteTodoAction(id)),
+        onTodoTapDelete: (id) => store.dispatch(DeleteTodoAction(id)),
+        ),
+      builder: (context, viewModel) {
+        return ListView.builder(
+          itemCount: viewModel.todos.length,
+          itemBuilder: (context, index){
+            return  Dismissible(
+              onDismissed: (direction) {
+                viewModel.onTodoTapDelete(index);
+              },
+              background: Container(
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.all(14),
+                color: Colors.red,
+                child: const Icon(Icons.delete, color: Colors.white),
+              ),
+              key: Key(viewModel.todos[index].task),
+              child: Card(
+                child: ListTile(
+                title: Text(viewModel.todos[index].task),
+                trailing: Checkbox(
+                  value: viewModel.todos[index].completed,
+                  onChanged: (value){
+                    print('de: $index');
+                    viewModel.onTodoTap(index);
+                  },
+                ),
+                ),
+              )
+              )
+            ;
+          }
+          );
+      }
+        
+      );
+  }
+
+}
+
+
+
+
+
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key, required this.store});
   final Store<AppState> store;
@@ -100,6 +279,7 @@ class MyApp extends StatelessWidget {
     return StoreProvider<AppState>(
         store: store,
         child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -123,13 +303,53 @@ class MyHomePage extends StatelessWidget {
 
   MyHomePage({super.key, required this.title, required this.store});
 
+  //Filter? selectedFilter;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Todo Redux')),
-      body: StoreConnector<AppState, List<Todo>>(
+          title: const Text('Todo Redux'),
+          actions: [
+            FilterLink(),
+           /*  StoreConnector<AppState, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(VisibilityFilter);
+              } ,
+              builder: (context, callback) {
+                return 
+                PopupMenuButton<Filter>(
+              initialValue: selectedFilter,
+              onSelected: (Filter item){
+                if (item == Filter.completed){
+                  callback();
+                  print('value:  $item');
+                }
+                
+              },
+              itemBuilder: (context) => <PopupMenuEntry<Filter>>[
+                const PopupMenuItem<Filter>(
+                  value: Filter.show,
+                  child: Text('All'),
+                  ),
+                const PopupMenuItem<Filter>(
+                  value: Filter.active,
+                  child: Text('Active'),
+                ),
+                const PopupMenuItem<Filter>(
+                  value: Filter.completed,
+                  child: Text('Completed'),
+                ),
+              ],
+            );
+              },
+              ), */
+          ],
+          ),
+      body: 
+      VisibleTodoList(),
+      /* StoreConnector<AppState, List<Todo>>(
         converter: (store) => store.state.todos.toList(),
         builder: (context, todos) {
           if (todos.isEmpty) {
@@ -206,7 +426,7 @@ class MyHomePage extends StatelessWidget {
             );
           }
         },
-      ),
+      ), */
       floatingActionButton: StoreConnector<AppState, VoidCallback>(
         converter: (store) {
           return () {
