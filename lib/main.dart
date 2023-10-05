c55803a290382c0c33245c490af2517d7640f8b0
Jairo Cruz <azaveliz@example.com>
2023-10-04 20:48:56 -0600

    add and configure core firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@ import 'package:redux/redux.dart';
 import 'package:todo_redux/redux/reducer.dart';
 import 'package:todo_redux/redux/state.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'myApp.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   final store = Store<AppState>(todosReducer, initialState: AppState());
   runApp(MyApp(
     store: store,
